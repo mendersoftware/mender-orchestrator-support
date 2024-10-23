032239fa3302bb6b68c710a6298c660d59a8ca90
Kristian Amlie <kristian.amlie@northern.tech>
2024-10-23 12:04:17 +0200

    docs(demo): Support manual installation without deb package.
    Ticket: MEN-7016
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- demo/pregenerated_env/manual_install.sh
+++ demo/pregenerated_env/manual_install.sh
@@ -19,15 +19,17 @@ log_and_execute() {
 
 
 
-log_and_execute mv orch-install/bin/mender-update-orchestrator /usr/bin/
+if [ $ORCH_CUSTOM_INSTALL = 1 ]; then
+    log_and_execute mv orch-install/bin/mender-update-orchestrator /usr/bin/
 
-# A modification needed to work on QEMU
-# An artifact of doing a manual installation as real installation isn't ready yet
-log_and_execute ln -s /usr/lib/ssl-3 /usr/lib/ssl
+    # A modification needed to work on QEMU
+    # An artifact of doing a manual installation as real installation isn't ready yet
+    log_and_execute ln -s /usr/lib/ssl-3 /usr/lib/ssl
 
-# Init the default data store
-log_and_execute mkdir /data/mender-update-orchestrator
-log_and_execute ln -s /data/mender-update-orchestrator /var/lib/mender-update-orchestrator
+    # Init the default data store
+    log_and_execute mkdir /data/mender-update-orchestrator
+    log_and_execute ln -s /data/mender-update-orchestrator /var/lib/mender-update-orchestrator
+fi
 
 log_and_execute cp -r orch-install/mock_env/ /data/mender-update-orchestrator/
 
